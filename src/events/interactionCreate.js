f1bdf1e50362900347aac965a3d7ea1965be87fb
Arviixzuh <victorgabriel725456@gmail.com>
2023-01-07 23:23:52 -0400

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,17 +1,18 @@
 const { EmbedBuilder, InteractionType } = require("discord.js");
 const { readdirSync } = require("fs");
 
- module.exports = {
-	name: 'interactionCreate',
-	execute: async(interaction) => {
-  let client = interaction.client;
-   if (!interaction.type == InteractionType.ApplicationCommand) return;
-   if(interaction.user.bot) return;
+module.exports = {
+  name: 'interactionCreate',
+  execute: async (interaction) => {
+    let client = interaction.client;
+    if (!interaction.type == InteractionType.ApplicationCommand) return;
+    if (interaction.user.bot) return;
 
-	readdirSync('./src/commands').forEach(file => {
-        const command = require(`../../src/commands/${file}`);
-        if(interaction.commandName === command.data.name) {
+    readdirSync('./src/commands').forEach(file => {
+      const command = require(`../../src/commands/${file}`);
+      if (interaction.commandName === command.data.name) {
         command.run(client, interaction)
-    }
-	})
-  }}
+      }
+    })
+  }
+}
