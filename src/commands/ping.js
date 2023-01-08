b70e8d892366e1e36f13103472885c0b1a882e5d
Arviixzuh <victorgabriel725456@gmail.com>
2023-01-07 23:22:51 -0400

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,9 +4,16 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("🏓 • Pong")
+    .setDescription("🏓 → Pong")
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-    run: async (client, interaction) => {
-      interaction.reply({ content: `**¡Pong!** 🏓\n**${Math.round(client.ws.ping)} ms**` })
-    }
- };
+  run: async (client, interaction) => {
+    const embed = new EmbedBuilder()
+      .setTitle('🏓 ¡Pong!')
+      .addFields({ name: '__Cliente:__', value: `\`\`\`cs\n${client.ws.ping} ms\`\`\`` })
+      .addFields({ name: '__Mensajes:__', value: `\`\`\`cs\n${Math.floor(interaction.createdTimestamp - Date.now())} ms\`\`\`` })
+      .setThumbnail("https://cdn.discordapp.com/attachments/909121957562314803/911329911556419604/ksRB6ZE.png")
+      .setColor("8bc091")
+    interaction.reply({ embeds: [embed] })
+
+  }
+};
