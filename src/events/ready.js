b264671e4947b1ce83afaa719fa2fa10784f770b
Arviixzuh <victorgabriel725456@gmail.com>
2023-01-07 23:24:27 -0400

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,7 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-    let activities = [`Dream Junk Studios`], i = 0;
-	setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Playing }), 22000);
-}};
+		let activities = [`Dream Junk Studios`], i = 0;
+		setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Playing }), 22000);
+	}
+};
